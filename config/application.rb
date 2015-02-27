460e812df5d8bff6d6fac848516bedd5bf733a00
Mike Chau <mchau@iwu.edu>
2015-02-27 03:35:33 -0600

    Enable cors

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,15 @@ module HaproxyPassengerStandaloneRollingUpgrades
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 1728000
+      end
+    end
   end
 end
